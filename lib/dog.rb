34d3724d57968deb66785b9168e493d10ad5cad5
Wendy Calderon <wendycalderon@gmail.com>
2019-06-27 23:55:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,18 +6,10 @@ class Dog
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
-  end
-  
-    attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name, mood = "nervous", owner)
-    @name = name
-    @mood = mood
     @owner = owner
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
